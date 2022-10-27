ff0c0485ac2d685592f0357ba36a20e60b5421f1
Muneerah <muneerah.aledaily@gmail.com>
2022-10-27 15:19:22 +0300

    Initial Commit

---
+++ MuneerahP/MuneerahPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuneerahPApp.swift
+//  MuneerahP
+//
+//  Created by Muneerah Aledaily on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MuneerahPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
